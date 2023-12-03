e96c0018126a44422b484661acb312e5cf55e3ce
Muhammad Adeeb Ansari <96252525+adeeb4098@users.noreply.github.com>
2023-12-03 15:22:18 +0530

    new_updates

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThreeScene from './ThreeScene';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <ThreeScene />
+    </div>
+  );
+};
+
+export default Home;
